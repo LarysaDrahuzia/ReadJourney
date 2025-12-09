daefc2e9dcdcec4e5c7cc945c860471ae0eef5d8
Larysa Drahuzia <draglara@ukr.net>
2025-12-09 19:04:47 +0200

    fix css RecommendedPage

---
+++ .history/src/redux/auth/slice_20251207155952.js
@@ -0,0 +1,100 @@
+import { createSlice } from '@reduxjs/toolkit';
+import {
+  loginUser,
+  registerUser,
+  logOut,
+  refreshUser,
+  addToMyLibrary,
+  removeFromMyLibrary,
+} from './operations.js';
+
+const handlePending = state => {
+  state.isRefreshing = true;
+};
+const handleReject = (state, { payload }) => {
+  state.isRefreshing = false;
+  state.error = payload;
+};
+
+const slice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: {
+      name: null,
+      email: null,
+      library: [],
+    },
+    token: null,
+    error: null,
+    isLoggedIn: false,
+    isRefreshing: false,
+  },
+  extraReducers: builder =>
+    builder
+      .addCase(registerUser.pending, handlePending)
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.error = null;
+        state.user.name = action.payload.name;
+        state.user.email = action.payload.email;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(registerUser.rejected, handleReject)
+      .addCase(loginUser.pending, handlePending)
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.error = null;
+        state.user.name = action.payload.name;
+        state.user.email = action.payload.email;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(loginUser.rejected, handleReject)
+      .addCase(logOut.fulfilled, state => {
+        state.error = null;
+        state.user = { name: null, email: null, library: [] };
+        state.token = null;
+        state.isLoggedIn = false;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.pending, handlePending)
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.error = null;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+
+        state.isRefreshing = false;
+        state.isLoggedIn = !!action.payload.token;
+      })
+      .addCase(refreshUser.rejected, state => {
+        state.isRefreshing = false;
+        state.user = { name: null, email: null, library: [] };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+
+      .addCase(addToMyLibrary.pending, handlePending)
+      .addCase(addToMyLibrary.fulfilled, (state, action) => {
+        state.error = null;
+        if (!state.user.library) {
+          state.user.library = [];
+        }
+        if (!state.user.library.includes(action.payload)) {
+          state.user.library.push(action.payload);
+        }
+        state.isRefreshing = false;
+      })
+      .addCase(addToMyLibrary.rejected, handleReject)
+      .addCase(removeFromMyLibrary.pending, handlePending)
+      .addCase(removeFromMyLibrary.fulfilled, (state, action) => {
+        state.error = null;
+        state.isRefreshing = false;
+        state.user.library = state.user.library.filter(
+          id => id !== action.payload
+        );
+      })
+      .addCase(removeFromMyLibrary.rejected, handleReject),
+});
+
+export default slice.reducer;
